a88bad921a51e30fdc4e8be3cb45489a1d07bc49
Vo Thanh Luan <vtluan@ninepoints.vn>
2014-01-22 12:05:29 +0700

    Java Common Helper

---
+++ JavaCommonHelper.java
@@ -0,0 +1,17 @@
+/**
+*
+* Check if a String is a numeric type in Java
+*
+*/
+public static boolean isNumeric(String str)  
+{  
+  try  
+  {  
+    double d = Double.parseDouble(str);  
+  }  
+  catch(NumberFormatException nfe)  
+  {  
+    return false;  
+  }  
+  return true;  
+}
